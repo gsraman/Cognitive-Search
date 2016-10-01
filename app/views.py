2f4cf4dadf12d943ba725b1f5c19133455510f8c
gsraman <gsraman1995@gmail.com>
2016-10-01 12:50:35 +0530

    add first version of concept map

--- app/views.py
+++ app/views.py
@@ -10,10 +10,24 @@ def index():
 
 @app.route('/search',methods=['POST'])
 def search():
+    i = 1;
+    j = 1;
+    payload = { "entities1": [] , "relations" : []}
     search_query =  request.form['search']
     wiki_page = wikipedia.page(str(search_query))
-
-    res = requests.post('http://node-red-gsraman.au-syd.mybluemix.net/wiki', data=)
+    wiki_content = wiki_page.content.encode('utf-8')
+    res = requests.post('http://node-red-gsraman.au-syd.mybluemix.net/wiki', data=wiki_content)
     if res.ok:
-        print res.json()
-    return "Sorry"
+        result = res.json()
+        if float(result['taxonomy'][0]['score']) > 0.3:
+            taxonomy = str(result['taxonomy'][0]['label']).split("/")
+            taxonomy.remove('')
+            for element in taxonomy:
+                payload["entities1"].append({"key": i , "value1" : element})
+                i+=1
+                if int(taxonomy.index(element)) != 0:
+                    payload["relations"].append({"key1": taxonomy.index(element)+1 , "key2": int(taxonomy.index(element)), "relation" : "belongs to" })
+            payload["entities1"].append({"key": i , "value1" : search_query })
+            payload["relations"].append({"key1": i , "key2": i-1, "relation" : "belongs to" })
+        return render_template('concept-map.html',payload=payload)
+    return "Sorry couldn't find the term"
